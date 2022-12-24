8ac3e75965856023bd975f53ccca37614fcddd44
DobbyCute <dobby.akhmadi@gmail.com>
2022-12-24 15:03:41 +0700

    add security config api gateway and discovery server

--- api-gateway-service/src/main/java/com/product/ApiGatewayService.java
+++ api-gateway-service/src/main/java/com/product/ApiGatewayService.java
@@ -2,8 +2,10 @@ package com.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayService {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayService.class, args);
