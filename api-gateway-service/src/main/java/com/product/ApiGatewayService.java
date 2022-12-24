5ddec2d75fb0bdbf9eea927833f63e741dee3d71
dobbyputra <dobby.akhmadi@gmail.com>
2022-12-24 11:51:49 +0700

    add api gateway and setting

--- api-gateway-service/src/main/java/com/product/ApiGatewayService.java
+++ api-gateway-service/src/main/java/com/product/ApiGatewayService.java
@@ -2,12 +2,10 @@ package com.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class DiscoveryService {
+public class ApiGatewayService {
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryService.class, args);
+        SpringApplication.run(ApiGatewayService.class, args);
     }
 }
